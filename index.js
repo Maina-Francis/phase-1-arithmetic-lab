044c33c0deb08405a8d4d86f763ad9f5c94cee0c
Francis Maina <sebastianfrancix@gmail.com>
2022-08-22 08:31:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.ceil(Math.random() * 10);
+console.log(random);
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(5, 17, 1, 7, 20);
